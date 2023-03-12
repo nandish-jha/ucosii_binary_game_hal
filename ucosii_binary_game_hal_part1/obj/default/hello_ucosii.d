4f6f97838d9d598c7b0f24250e11ccf341195eb3
nandish-jha <nandish-jha@gmail.com>
2023-03-12 15:43:00 -0600

    Everything seems to be working now, finishing up

---
+++ ucosii_binary_game_hal_part1/obj/default/hello_ucosii.d
@@ -0,0 +1,37 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ ../ucosii_binary_game_hal_part1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/io.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/alt_types.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/includes.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/os_cpu.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_irq.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/nios2.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/alt_types.h \
+ ../ucosii_binary_game_hal_part1_bsp/system.h \
+ ../ucosii_binary_game_hal_part1_bsp/linker.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_irq.h \
+ ../ucosii_binary_game_hal_part1_bsp//UCOSII/inc/os_cfg.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_alarm.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_llist.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/priv/alt_alarm.h \
+ ../ucosii_binary_game_hal_part1_bsp//UCOSII/inc/ucos_ii.h \
+ ../ucosii_binary_game_hal_part1_bsp//HAL/inc/os_cpu.h
+../ucosii_binary_game_hal_part1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/io.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/alt_types.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/includes.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/os_cpu.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_irq.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/nios2.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/alt_types.h:
+../ucosii_binary_game_hal_part1_bsp/system.h:
+../ucosii_binary_game_hal_part1_bsp/linker.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/priv/alt_legacy_irq.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_irq.h:
+../ucosii_binary_game_hal_part1_bsp//UCOSII/inc/os_cfg.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_alarm.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/sys/alt_llist.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/priv/alt_alarm.h:
+../ucosii_binary_game_hal_part1_bsp//UCOSII/inc/ucos_ii.h:
+../ucosii_binary_game_hal_part1_bsp//HAL/inc/os_cpu.h:
---
+++ ucosii_binary_game_hal_part2/obj/default/hello_ucosii.d
@@ -0,0 +1,52 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ ../ucosii_binary_game_hal_part2_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_dev.h \
+ ../ucosii_binary_game_hal_part2_bsp/system.h \
+ ../ucosii_binary_game_hal_part2_bsp/linker.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_llist.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/alt_types.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_alarm.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_llist.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/priv/alt_alarm.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_warning.h \
+ ../ucosii_binary_game_hal_part2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/io.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/alt_types.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/includes.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/os_cpu.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_irq.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/nios2.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_irq.h \
+ ../ucosii_binary_game_hal_part2_bsp//UCOSII/inc/os_cfg.h \
+ ../ucosii_binary_game_hal_part2_bsp//UCOSII/inc/ucos_ii.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/os_cpu.h globals.h \
+ ../ucosii_binary_game_hal_part2_bsp//drivers/inc/altera_up_avalon_character_lcd_regs.h \
+ ../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_stdio.h
+../ucosii_binary_game_hal_part2_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_dev.h:
+../ucosii_binary_game_hal_part2_bsp/system.h:
+../ucosii_binary_game_hal_part2_bsp/linker.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_llist.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/alt_types.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/priv/alt_dev_llist.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_alarm.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_llist.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/priv/alt_alarm.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_warning.h:
+../ucosii_binary_game_hal_part2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/io.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/alt_types.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/includes.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/os_cpu.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_irq.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/nios2.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/priv/alt_legacy_irq.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_irq.h:
+../ucosii_binary_game_hal_part2_bsp//UCOSII/inc/os_cfg.h:
+../ucosii_binary_game_hal_part2_bsp//UCOSII/inc/ucos_ii.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/os_cpu.h:
+globals.h:
+../ucosii_binary_game_hal_part2_bsp//drivers/inc/altera_up_avalon_character_lcd_regs.h:
+../ucosii_binary_game_hal_part2_bsp//HAL/inc/sys/alt_stdio.h:
